79be48b91d58ba1354035eeed6d6914977ca1a60
Mateusz Majka <mateusz.majka@iteo.com>
2023-02-13 12:07:09 +0100

    Initial commit

---
+++ src/main/kotlin/kmm/sample/Application.kt
@@ -0,0 +1,21 @@
+package kmm.sample
+
+import io.ktor.server.application.*
+import io.ktor.server.cio.*
+import io.ktor.server.engine.*
+import kmm.sample.plugins.*
+
+fun main() {
+    embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureDatabases()
+    configureTemplating()
+    configureMonitoring()
+    configureHTTP()
+    configureSecurity()
+    configureRouting()
+}
